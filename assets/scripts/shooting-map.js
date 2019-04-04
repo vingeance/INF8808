fd106c62e0054a2b57a1e38d2d2d2bf13f7dcab8
Francis Raymond <francis.raymond.938@gmail.com>
2019-04-03 22:43:32 -0400

    Map district focus in progress

--- assets/scripts/shooting-map.js
+++ assets/scripts/shooting-map.js
@@ -6,86 +6,147 @@
 //   .attr("x", "25%")
 //   .attr("y", "25%");
 
+function moveToFront() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  })
+}
+
 function create_shooting_map(parent, width, height, sources, color) {
 
-  var projection = d3.geoMercator().center([ -73.75,45.58 ]).scale([ 100000 ]).translate([width/2, height/2]);
+  var permitsByDistrict = {}
+
+  sources.forEach(type => {
+    type.values.forEach(production => {
+      production.values.forEach(district => {
+        permitsByDistrict[district.key] = permitsByDistrict[district.key] || []
+
+        district.values.forEach(location => {
+          permitsByDistrict[district.key].push({ typeKey: type.key, ...location })
+        })
+      })
+    })
+  })
+
+  var projection = d3.geoMercator().center([ -73.75,45.58 ]).scale([ 80000 ]).translate([width/2, height/2]);
   var path = d3.geoPath().projection(projection);
+
+  var focusedProjection = d3.geoMercator().center([ -73.75,45.58 ]).scale([ 250000 ]).translate([width/2, height/2]);
+  var focusedPath = d3.geoPath().projection(focusedProjection);
 
   d3.json("./data/montrealTerreGeo.json")
     .then(function(geojson) {
 
+      var tooltip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(d => `${d.TITRE_PRODUCTION} (${d.DUREE_DE.split("-")[0]})`)
+
+      parent.call(tooltip)
+
+      var focusedDistrict
+
+      function getPath(d) {
+        return d.properties.NOM === focusedDistrict ? focusedPath(d) : path(d)
+      }
+
+      function getTransform(d) {
+        return "translate("
+          + (d.NOM_ARROND === focusedDistrict ? focusedProjection([d.LONGITUDE, d.LATITUDE]) : projection([d.LONGITUDE, d.LATITUDE]))
+          + ")"
+      }
+
       var graph = parent.append("g");
+
       graph.selectAll("path")
         .data(geojson.features)
         .enter()
         .append("path")
-        .attr("id", function(d) {
-          return d.properties["NUM"];
-        })
-        .attr("name", function(d) {
-          return d.properties["NOM"];
-        })
+        .attr("id", d => `${d.properties.NOM}-district`)
+        .attr("name", d => d.properties.NOM)
         .attr("class", "zone")
-        .attr("d", path)
+        .attr("d", getPath)
         .attr("fill", "white")
         .on("click", function(d) {
           showPanel(d, sources);
-        });
 
-      // var tooltip = parent.append("div")
-      //   .attr("class", "tooltip")
-      //   .style("opacity", 0)
-      var tooltip = d3.tip()
-        .attr('class', 'd3-tip')
-        .offset([-10, 0])
-        .html(d => `${d.values[0].TITRE_PRODUCTION} (${d.values[0].DUREE_DE.split("-")[0]})`)
+          focusedDistrict = d.properties.NOM
 
-      parent.call(tooltip)
+          var district = graph.select(`#${d.properties.NOM}-district`)
+          district.attr("d", getPath)
+          moveToFront.call(district)
 
-      // Create a sub 'g' for each prod types groups
-      const prodTypesGroupsEnter = graph.selectAll("g")
-        .data(sources)
+          // TODO this doesn't seem to work
+          var circles = graph.select(`${d.properties.NOM}-circles`).selectAll("circle")
+          circles.attr("d", getTransform)
+          moveToFront.call(circles)
+        })
+
+      graph.selectAll("g")
+        .data(geojson.features)
         .enter()
         .append("g")
-        .attr("id", function(d) {
-          return "prodGroup" + d.key;
-        })
-        .attr("groupId", function(d) {
-          return d.key;
-        });
-
-      // Create a sub 'g' for each prod ids groups (a particular prod may have been shot at different locations)
-      const prodIdsGroupsEnter = prodTypesGroupsEnter.merge(prodTypesGroupsEnter).selectAll("g")
-        .data(d => d.values)
+        .attr("id", d => `${d.properties.NOM}-circles`)
+        .selectAll("circle")
+        .data(d => permitsByDistrict[d.properties.NOM] || [])
         .enter()
-        .append("g")
-        .attr("id", function(d) {
-          return "prodId" + d.key;
+        .append("circle")
+        .attr("class", "circle")
+        .attr("r", 1)
+        .attr("transform", getTransform)
+        .attr("fill", function(d) {
+          return color(d.typeKey)
         })
-        .attr("prodName", function(d) {
-          return d.NOM_PRO;
-        });
+        .on("mouseover", function(d) {
+          tooltip.show.call(this, d)
+        })
+        .on("mouseout", tooltip.hide)
 
-      // Create a circle for each shootings
-      const circle = prodIdsGroupsEnter.merge(prodIdsGroupsEnter).selectAll("circle")
-        .data(d => d.values)
-        .enter()
-          .append("circle")
-          .attr("class", "circle")
-          .attr("name", function(d) {
-            return d.values[0].NOM_ARROND;
-          })
-          .attr("r", 1.5)
-          .attr("transform", function(d) {
-            return "translate(" + projection([d.values[0].LONGITUDE, d.values[0].LATITUDE]) + ")";
-          })
-          .attr("fill", function(d) {
-              return color(d3.select(this.parentNode.parentNode).attr("groupId"));
-          })
-          .on("mouseover", function(d) {
-            tooltip.show.call(this, d)
-          })
-          .on("mouseout", tooltip.hide)
+
+      // // Create a sub 'g' for each prod types groups
+      // const prodTypesGroupsEnter = graph.selectAll("g")
+      //   .data(sources)
+      //   .enter()
+      //   .append("g")
+      //   .attr("id", function(d) {
+      //     return "prodGroup" + d.key;
+      //   })
+      //   .attr("groupId", function(d) {
+      //     return d.key;
+      //   });
+
+      // // Create a sub 'g' for each prod ids groups (a particular prod may have been shot at different locations)
+      // const prodIdsGroupsEnter = prodTypesGroupsEnter.merge(prodTypesGroupsEnter).selectAll("g")
+      //   .data(d => d.values)
+      //   .enter()
+      //   .append("g")
+      //   .attr("id", function(d) {
+      //     return "prodId" + d.key;
+      //   })
+      //   .attr("prodName", function(d) {
+      //     return d.NOM_PRO;
+      //   });
+
+      // // Create a circle for each shootings
+      // const circle = prodIdsGroupsEnter.merge(prodIdsGroupsEnter).selectAll("circle")
+      //   .data(d => d.values)
+      //   .enter()
+      //     .append("circle")
+      //     .attr("class", "circle")
+      //     .attr("name", function(d) {
+      //       return d.values[0].NOM_ARROND;
+      //     })
+      //     .attr("r", 1.5)
+      //     .attr("transform", function(d) {
+      //       return "translate(" + projection([d.values[0].LONGITUDE, d.values[0].LATITUDE]) + ")";
+      //     })
+      //     .attr("fill", function(d) {
+      //         return color(d3.select(this.parentNode.parentNode).attr("groupId"));
+      //     })
+      //     .on("mouseover", function(d) {
+      //       tooltip.show.call(this, d)
+      //     })
+      //     .on("mouseout", tooltip.hide)
     });
 
     // Create the map legend
